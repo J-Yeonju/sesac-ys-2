cd4c54a4e9f581675b673600f573b968012dfeee
J-Yeonju <Yeonju.dev@gmail.com>
2023-12-21 16:44:31 +0900

    study: socketIO chatting

--- react-proj/11-socket/src/App.js
+++ react-proj/11-socket/src/App.js
@@ -1,14 +1,21 @@
-import Practice1 from './components/Practice1';
-import './App.css';
-import Chatting1 from './components/Chatting1';
+import "./App.css";
+import Chatting3 from "./components/Chatting3";
+// import Chatting1 from "./components/Chatting1";
+// import Chatting2 from "./components/Chatting2";
+// import Practice1 from "./components/Practice1";
+
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      {/* 실습 1 */}
       {/* <Practice1 /> */}
 
-      {/* 채팅실습 */}
-      <Chatting1 />
+      {/* 채팅 실습 */}
+      {/* 2, 3번 */}
+      {/* <Chatting1 /> */}
+      {/* 3-1, 3-2, 3-3번 */}
+      <Chatting3 />
     </div>
   );
 }
